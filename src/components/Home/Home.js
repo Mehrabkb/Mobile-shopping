c91c83e30b70583f7a3f45d48bb2c6743cf32682
mehrabkb <mkordbacheh@gmail.com>
2023-12-24 14:54:41 +0330

    add reset stylesheet and font

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Home.css';
+
+const Home = () => (
+  <div>
+    یک تست است
+  </div>
+);
+
+Home.propTypes = {};
+
+Home.defaultProps = {};
+
+export default Home;
