51f3c094662139ee50f92c60fa984015ee4810cb
mehrabkb <mkordbacheh@gmail.com>
2023-12-25 11:46:08 +0330

    header dynamic list worked

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,36 +3,46 @@ import PropTypes from 'prop-types';
 import './Header.css';
 import logo from '../../images/logo.jpg';
 
+class Header extends Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuList : ['خانه' , 'محصولات' , 'تماس با ما' , 'درباره ما'],
+            list : []
+        }
+    }
 
-const menuList = ['خانه' , 'محصولات' , 'تماس با ما' , 'درباره ما']
-let list = [];
+    componentDidMount() {
+        let temp_list = [];
+        this.state.menuList.forEach((item , index) =>{
+            temp_list.push(<li>
+                <a href={'https://google.com'}>{item}</a></li>)
+        });
+        this.setState({list : temp_list});
+        console.log('list : ' + this.state.list);
+    }
 
-menuList.forEach((item , index) =>{
-   list.push(<li>
-       <a href={'https://google.com'}>{item}</a></li>)
-});
-
-const Header = () => (
-    <header className={'header'}>
-        <div className={'user-box'}>
-            <a href={'https://google.com'}>پنل کاربری</a>
-        </div>
-        <nav>
-            <ul>
-                {list}
-            </ul>
-        </nav>
-        <div className={'logo'}>
-            <a>
-                <img src={logo}/>
-            </a>
-        </div>
-    </header>
-);
-
-Header.propTypes = {};
-
-Header.defaultProps = {};
-
+render(){
+    const {menuList , list} = this.state;
+    console.log(list);
+       return  <header className={'header'}>
+            <div className={'user-box'}>
+                <a href={'https://google.com'}>پنل کاربری</a>
+            </div>
+            <nav>
+                <ul>
+                    {list}
+                </ul>
+            </nav>
+            <div className={'logo'}>
+                <a>
+                    <img src={logo}/>
+                </a>
+            </div>
+        </header>
+    }
+}
 export default Header;
+
+
